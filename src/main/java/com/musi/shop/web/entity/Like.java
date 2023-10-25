c78cb32029562fca1a5abcb24144c98cfe4eb296
Kim-Dayeong <kkimdayeongg@gmail.com>
2023-10-25 18:35:24 +0900

    좋아요 엔티티 생성

---
+++ src/main/java/com/musi/shop/web/entity/Like.java
@@ -0,0 +1,23 @@
+package com.musi.shop.web.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity(name = "Like")
+@NoArgsConstructor
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Boolean Like;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Member member;
+
+}
